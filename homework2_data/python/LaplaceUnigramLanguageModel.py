a562ffc3f445a674bbdd4fe3a29f11df4e0157b0
chenxu001 <chenxu001@outlook.com>
2018-09-13 17:49:41 +0800

    fix indent

--- homework2_data/python/LaplaceUnigramLanguageModel.py
+++ homework2_data/python/LaplaceUnigramLanguageModel.py
@@ -2,40 +2,40 @@ import math, collections
 
 class LaplaceUnigramLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.laplaceunigramCounts = collections.defaultdict(lambda: 0)
-    self.total = 0
-    self.train(corpus)
+    def __init__(self, corpus):
+        """Initialize your data structures in the constructor."""
+        # TODO your code here
+        self.laplaceunigramCounts = collections.defaultdict(lambda: 0)
+        self.total = 0
+        self.train(corpus)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    for sentence in corpus.corpus:
-      for datum in sentence.data:  
-        token = datum.word
-        self.laplaceunigramCounts[token] += 1
-        self.total += 1
+    def train(self, corpus):
+        """ Takes a corpus and trains your language model. 
+                Compute any counts or other corpus statistics in this function.
+        """    
+        # TODO your code here
+        for sentence in corpus.corpus:
+            for datum in sentence.data:    
+                token = datum.word
+                self.laplaceunigramCounts[token] += 1
+                self.total += 1
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    score = 0.0
-    number_of_unseen = 0
-    for token in sentence:
-      if self.laplaceunigramCounts[token] == 0:
-        number_of_unseen += 1
-    if number_of_unseen > 0:
-      for t in self.laplaceunigramCounts:
-        self.laplaceunigramCounts[t] += 1
-      self.total += len(self.laplaceunigramCounts)      
-    for token in sentence:
-      count = self.laplaceunigramCounts[token]
-      score += math.log(count)
-      score -= math.log(self.total)
-    return score
+    def score(self, sentence):
+        """ Takes a list of strings as argument and returns the log-probability of the 
+                sentence using your language model. Use whatever data you computed in train() here.
+        """
+        # TODO your code here
+        score = 0.0
+        number_of_unseen = 0
+        for token in sentence:
+            if self.laplaceunigramCounts[token] == 0:
+                number_of_unseen += 1
+        if number_of_unseen > 0:
+            for t in self.laplaceunigramCounts:
+                self.laplaceunigramCounts[t] += 1
+            self.total += len(self.laplaceunigramCounts)            
+        for token in sentence:
+            count = self.laplaceunigramCounts[token]
+            score += math.log(count)
+            score -= math.log(self.total)
+        return score
