1f002cb4e4ecae14ce6b0b95a8ea01970116f97e
chenxu001 <chenxu001@outlook.com>
2018-09-19 11:58:50 +0800

    change laplaceBigram code

--- homework2_data/python/LaplaceBigramLanguageModel.py
+++ homework2_data/python/LaplaceBigramLanguageModel.py
@@ -7,7 +7,6 @@ class LaplaceBigramLanguageModel:
         # TODO your code here
         self.laplace_unigram_counts = collections.defaultdict(lambda: 0) # one word dict
         self.laplace_bigram_counts = collections.defaultdict(lambda: 0) # two words dict
-        self.same_pre = collections.defaultdict(lambda: []) # list of second word with same previous word
         self.train(corpus)
 
     def train(self, corpus):
@@ -21,7 +20,6 @@ class LaplaceBigramLanguageModel:
                 la_token = sentence.data[i+1].word
                 self.laplace_unigram_counts[pre_token] += 1
                 self.laplace_bigram_counts[(pre_token, la_token)] += 1
-                self.same_pre[pre_token].append(la_token)
 
     def score(self, sentence):
         """ Takes a list of strings as argument and returns the log-probability of the 
@@ -29,17 +27,14 @@ class LaplaceBigramLanguageModel:
         """
         # TODO your code here
         score = 0.0
-        pre_add_list = []        
+        number_of_unseen = 0
         for i in range(len(sentence)-1):
             if self.laplace_bigram_counts[(sentence[i], sentence[i+1])] == 0:
+                number_of_unseen += 1
+        if number_of_unseen > 0:
+            for i in range(len(sentence)-1):
                 self.laplace_bigram_counts[(sentence[i], sentence[i+1])] += 1
-                self.laplace_unigram_counts[sentence[i]] += 1
-                pre_add_list.append(sentence[i])
-        
-        for i in set(pre_add_list):
-            self.laplace_unigram_counts[i] += len(set(self.same_pre[i]))
-            for j in set(self.same_pre[i]):
-                self.laplace_bigram_counts[(i, j)] += 1
+                self.laplace_unigram_counts[sentence[i]] += len(self.laplace_unigram_counts)
                 
         for i in range(len(sentence)-1):
             score += math.log(self.laplace_bigram_counts[(sentence[i], sentence[i+1])])
